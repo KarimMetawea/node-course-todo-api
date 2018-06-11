076079f74cbd9478815e0c86356f715ed245b185
karim <karim.metawea@gmail.com>
2018-06-11 13:06:34 -0700

    run with uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect('mongodb://karimMetawea:karim1961993@ds155730.mlab.com:55730/todo-api' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
