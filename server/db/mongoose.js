562168b0298dd71a5273ab8dd4dc1dd51e065f12
karim <karim.metawea@gmail.com>
2018-06-11 12:14:06 -0700

    run with mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://karimMetawea:karim1961993@ds155730.mlab.com:55730/todo-api');
-mongoose.connection.once('open',() => {
-    console.log('connected to the data base');
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
