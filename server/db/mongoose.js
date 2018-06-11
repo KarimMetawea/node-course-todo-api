f45c09d356ce3798edb4d8c1a78870957b205b56
karim <karim.metawea@gmail.com>
2018-06-11 12:05:53 -0700

    run with mlab uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('MONGODB_URI');
+mongoose.connect('mongodb://karimMetawea:karim1961993@ds155730.mlab.com:55730/todo-api');
 mongoose.connection.once('open',() => {
     console.log('connected to the data base');
 });
